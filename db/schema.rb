56ccc78b99357498bfd498c34fbd21bab90c8b14
ZakharDay <zakhar.day@gmail.com>
2018-11-09 13:08:27 +0300

    Schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_152708) do
   end
 
   create_table "documents", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -33,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_152708) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "login"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
